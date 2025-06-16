1501545446b42415ca28496380b5bd3b3aa0a9d0
Gabriel Camargo <gcamargo.dev@gmail.com>
2025-06-16 19:00:29 -0300

    feat: create duracaoEmMinutos method on Musica class

--- src/entities/Musica.java
+++ src/entities/Musica.java
@@ -3,20 +3,22 @@ package entities;
 import enums.MusicaGenero;
 
 public class Musica extends Midia {
-	private double duracao;
+	private int duracao;
 	private int streams;
 
-	public Musica(String nome, MusicaGenero genero, int anoLancamento, double duracao, int streams) {
+	public Musica(String nome, MusicaGenero genero, int anoLancamento, int duracao, int streams) {
 		super(nome, anoLancamento, genero);
 		this.duracao = duracao;
 		this.streams = streams;
 	}
 
-	public double getDuracao() {
-		return duracao;
-	}
-
 	public int getStreams() {
 		return streams;
+	}
+
+	public String duracaoEmMinutos() {
+		int minutos = duracao / 60;
+		int segundos = duracao % 60;
+		return String.format("%d:%02d", minutos, segundos);
 	}
 }
