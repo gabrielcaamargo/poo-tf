304f672f4311210024e11f52ab4182aea14c278b
Gabriel Camargo <gcamargo.dev@gmail.com>
2025-06-16 18:58:39 -0300

    feat: integrate Midia class on Musica and Album

--- src/entities/Musica.java
+++ src/entities/Musica.java
@@ -2,29 +2,18 @@ package entities;
 
 import enums.MusicaGenero;
 
-public class Musica {
-	private String nome;
+public class Musica extends Midia {
 	private double duracao;
-	private MusicaGenero genero;
 	private int streams;
 
-	public Musica(String nome, double duracao, MusicaGenero genero, int streams) {
-		this.nome = nome;
+	public Musica(String nome, MusicaGenero genero, int anoLancamento, double duracao, int streams) {
+		super(nome, anoLancamento, genero);
 		this.duracao = duracao;
-		this.genero = genero;
 		this.streams = streams;
-	}
-
-	public String getNome() {
-		return nome;
 	}
 
 	public double getDuracao() {
 		return duracao;
-	}
-
-	public MusicaGenero getGenero() {
-		return genero;
 	}
 
 	public int getStreams() {
